1725cfd5764b9f3180e529e21fbfea64e6d9c58a
ominestre <ominestre@gmail.com>
2022-11-16 14:46:05 -0500

    Setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: 'airbnb-base',
+  env: {
+    node: true
+  },
+  rules: {
+    'arrow-parens': 'off',
+    'no-multiple-empty-lines': ['error', {
+      max: 2
+    }],
+    'no-underscore-dangle': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'implicit-arrow-linebreak': 'off',
+  },
+};
