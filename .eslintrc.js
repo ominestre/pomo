4614433cb765d847d266a259682b6c41703d2e74
ominestre <ominestre@gmail.com>
2022-11-21 13:53:50 -0500

    Linting the lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
   extends: 'airbnb-base',
   env: {
-    node: true
+    node: true,
   },
   rules: {
     'arrow-parens': 'off',
     'no-multiple-empty-lines': ['error', {
-      max: 2
+      max: 2,
     }],
     'no-underscore-dangle': 'off',
     'import/no-unresolved': 'off',
