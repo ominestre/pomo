13aa9398021129ecd6b020244bd48be8cb3fb79a
ominestre <ominestre@gmail.com>
2022-11-23 11:40:37 -0500

    Rebuilds Timer component
    - Uses internal state over redux. At this time I'm a little
    inexperienced with react hooks and the errors produced
    from using redux was not worth the headache of pushing
    this components state to a global state store. It feels better using
    independent internal state. Keeping redux for now because I like
    the tool and I'd like to at least play with in the react space, but it
    doesn't feel like the right tool for this job going forward.
    
    - Uses react component class over function

--- src/features/timer/Timer.tsx
+++ src/features/timer/Timer.tsx
@@ -1,68 +1,101 @@
 import React from 'react';
 
-import { useAppSelector, useAppDispatch } from '../../state/hooks';
-import {
-  selectTime,
-  selectIsTimerActive,
-  decrementTimer,
-  stopTimer,
-} from './timer.state';
+interface TimerProps {}
 
-function Timer() {
-  const dispatch = useAppDispatch();
+interface TimerState {
+  minutes: number,
+  seconds: number,
+  isTimerActive: boolean,
+  intervalID: ReturnType<typeof setInterval> | null,
+}
 
-  let timerIntervalID: ReturnType<typeof setInterval>;
+const padLeadingZeros = (val: number | string): string =>
+  `00${val}`.slice(-2);
 
-  const startTimerHandler = () => {
-    timerIntervalID = setInterval(() => {
-      const isTimerActive = useAppSelector(selectIsTimerActive);
+class Timer extends React.Component<TimerProps, TimerState> {
+  constructor(props: TimerProps) {
+    super(props);
 
-      if (isTimerActive) {
-        dispatch(decrementTimer());
+    this.state = {
+      minutes: 25,
+      seconds: 0,
+      isTimerActive: false,
+      intervalID: null,
+    };
+  }
 
-        const { seconds, minutes } = useAppSelector(selectTime);
+  tick = () => {
+    if (this.state.seconds === 0) {
+      if (this.state.minutes === 0) this.setState({ minutes: 0, seconds: 0 });
 
-        if (minutes === 0 && seconds === 0) {
-          dispatch(stopTimer());
-          clearInterval(timerIntervalID);
-        }
+      else {
+        this.setState(state => ({
+          minutes: state.minutes - 1,
+          seconds: 59,
+        }));
       }
-    }, 1000);
+    } else {
+      this.setState(state => ({
+        seconds: state.seconds - 1,
+      }));
+    }
   };
 
-  const padLeadingZeros = (val: number | string): string =>
-    `00${val}`.slice(-2);
+  formatTime = () => {
+    const minutes = padLeadingZeros(this.state.minutes);
+    const seconds = padLeadingZeros(this.state.seconds);
+    return `${minutes}:${seconds}`;
+  };
 
-  const { seconds, minutes } = useAppSelector(selectTime);
-  const formattedTime = `${padLeadingZeros(minutes)}:${padLeadingZeros(seconds)}`;
+  startTimer = () => {
+    const intervalID = setInterval(() => {
+      if (this.state.isTimerActive) this.tick();
+      else this.stopTimer();
+    }, 1000);
 
-  return (
-    <div
+    this.setState({ isTimerActive: true, intervalID });
+  };
+
+  stopTimer = () => {
+    if (this.state.intervalID !== null) clearInterval(this.state.intervalID);
+
+    this.setState({ isTimerActive: false, intervalID: null });
+  };
+
+  resetTimer = () => {
+    this.stopTimer();
+    this.setState({ minutes: 25, seconds: 0 });
+  };
+
+  render() {
+    return <div
       id="pomodoro-timer"
       className="container-fluid d-flex justify-content-center text-bg-secondary"
     >
-      <span className="pomodoro-timer__time-display p-5 fs-1">{formattedTime}</span>
+      <span className="pomodoro-timer__time-display p-5 fs-1">{this.formatTime()}</span>
 
       <button
         className="pomodoro-timer__start-timer btn btn-success m-1 align-self-center"
-        onClick={() => startTimerHandler()}
+        onClick={this.startTimer}
       >
         Start
       </button>
 
       <button
         className="pomodoro-timer__stop-timer btn btn-danger m-1 align-self-center"
+        onClick={this.stopTimer}
       >
         Stop
       </button>
 
       <button
         className="pomodoro-timer__reset-timer btn btn-warning m-1 align-self-center"
+        onClick={this.resetTimer}
       >
         Reset
       </button>
-    </div>
-  );
+    </div>;
+  }
 }
 
 export default Timer;
