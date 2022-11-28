d5f934098bfb9d5dccafd472f296f199b3b068e6
ominestre <ominestre@gmail.com>
2022-11-28 16:14:29 -0500

    Updates style times
    - Buttons will stay grouped in smaller viewports

--- src/features/timer/Timer.tsx
+++ src/features/timer/Timer.tsx
@@ -32,30 +32,34 @@ export const Timer = () => {
   return (
     <div
       id="pomodoro-timer"
-      className="container-fluid d-flex justify-content-center text-bg-secondary"
+      className="container-fluid d-flex flex-wrap justify-content-center text-bg-secondary py-5"
     >
-      <span className="pomodoro-timer__time-display p-5 fs-1">{formatTime()}</span>
+      <div className="pomodoro-timer__timer-display">
+        <span className="pomodoro-timer__time-display px-5 fs-1 fw-semibold">{formatTime()}</span>
+      </div>
 
-      <button
-        className="pomodoro-timer__start-timer btn btn-success m-1 align-self-center"
-        onClick={startTimer}
-      >
-        Start
-      </button>
+      <div className="pomodoro-timer__controls d-flex flex-nowrap">
+        <button
+          className="pomodoro-timer__start-timer btn btn-success m-1 align-self-center"
+          onClick={startTimer}
+        >
+          Start
+        </button>
 
-      <button
-        className="pomodoro-timer__stop-timer btn btn-danger m-1 align-self-center"
-        onClick={stopTimer}
-      >
-        Stop
-      </button>
+        <button
+          className="pomodoro-timer__stop-timer btn btn-danger m-1 align-self-center"
+          onClick={stopTimer}
+        >
+          Stop
+        </button>
 
-      <button
-        className="pomodoro-timer__reset-timer btn btn-warning m-1 align-self-center"
-        onClick={resetTimer}
-      >
-        Reset
-      </button>
+        <button
+          className="pomodoro-timer__reset-timer btn btn-warning m-1 align-self-center"
+          onClick={resetTimer}
+        >
+          Reset
+        </button>
+      </div>
     </div>
   );
 };
