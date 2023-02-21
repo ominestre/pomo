46c1ac940028a2c8aec132968cef263c0c949cc9
ominestre <ominestre@gmail.com>
2023-02-21 14:19:42 -0500

    Rewrites Timer feature to use MaterialUI

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import ReactDOM from 'react-dom/client';
 import { store } from './state/store';
 import './index.scss';
