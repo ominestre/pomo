7feede6a0ed5e036ea148b34370f6d3f045f6285
ominestre <ominestre@gmail.com>
2023-03-01 12:23:55 -0500

    Fixes sourcemaps
    Prior to this change they were resolving to their post TS
    compiled state. This change maps them all the way to
    their original source.

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/bash
 
+export NODE_ENV="production"
+
 version=`head -n 1 CHANGELOG.md | cut -c4-`
 
 echo "creating v$version"
 
 # `-f` flag used to force inclusion of `CHANGELOG.md` changes
 npm version -f $version
+
+unset NODE_ENV
