ae5da1d0b8073b36d33ef9114ffb2753f92f3ee8
ominestre <ominestre@gmail.com>
2023-03-01 13:03:18 -0500

    Changes release script to also handle running build

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,9 +4,13 @@ export NODE_ENV="production"
 
 version=`head -n 1 CHANGELOG.md | cut -c4-`
 
-echo "creating v$version"
+echo "running production build"
+npm run build
 
+echo "creating v$version"
 # `-f` flag used to force inclusion of `CHANGELOG.md` changes
 npm version -f $version
+
+echo "ready for push with `git push main` and `git push --tags`"
 
 unset NODE_ENV
